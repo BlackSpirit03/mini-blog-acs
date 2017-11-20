87a70683672b11fded3b3450f39fc3ce05ff0807
Steve <Lormelet01@hotmail.fr>
2017-11-20 11:58:54 +0100

    modif
    modif

--- site/index.php
+++ site/index.php
@@ -1,3 +1,25 @@
+<?php
+	//Ouvrir la session
+	session_start();
+
+	//Connexion base de données (bdd_miniblog / admin_miniblog / Ten96wabuQ10sztU)
+	try{
+		$bdd = new PDO('mysql:host=localhost;dbname=bdd_miniblog', 'root', '');
+	}
+	catch(Exception $e){
+		die('Erreur : '.$e->getMessage());
+	}
+
+	//Affichage de la page nécessaire
+	if (empty($_GET["page"])){
+		require_once "./controllers/userauth.php";
+	}else{
+		if ($_GET['page'] == 'articleslist') {
+				require_once "./controllers/articleslist.php";
+			}
+	}
+?>
+
 <!DOCTYPE html>
 	<html>
 		<head>
@@ -5,27 +27,7 @@
 			<meta charset="utf-8">
 		</head>
 		<body>
-			<?php
-			//Ouvrir la session
-			session_start();
-
-			//Connexion base de données (bdd_miniblog / admin_miniblog / Ten96wabuQ10sztU)
-			try{
-				$bdd = new PDO('mysql:host=localhost;dbname=bdd_miniblog', 'root', '');
-			}
-			catch(Exception $e){
-				die('Erreur : '.$e->getMessage());
-			}
-
-			//Affichage de la page nécessaire
-			if (empty($_GET["page"])){
-				require_once "./controllers/userauth.php";
-			}else{
-				if ($_GET['page'] == 'articleslist') {
-						require_once "./controllers/articleslist.php";
-					}
-			}
-		?>
+			
 		</body>
 	</html>
 
