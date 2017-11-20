d576db90013b18d4e8382d315f133272ce7b2a6e
Steve <Lormelet01@hotmail.fr>
2017-11-20 11:27:39 +0100

    ajout onglet
    ajout onglet

--- site/index.php
+++ site/index.php
@@ -1,21 +1,35 @@
-<?php
-	//Ouvrir la session
-	session_start();
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Accueil du Blog</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php
+			//Ouvrir la session
+			session_start();
 
-	//Connexion base de données (bdd_miniblog / admin_miniblog / Ten96wabuQ10sztU)
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=bdd_miniblog', 'root', '');
-	}
-	catch(Exception $e){
-		die('Erreur : '.$e->getMessage());
-	}
-
-	//Affichage de la page nécessaire
-	if (empty($_GET["page"])){
-		require_once "./controllers/userauth.php";
-	}else{
-		if ($_GET['page'] == 'articleslist') {
-				require_once "./controllers/articleslist.php";
+			//Connexion base de données (bdd_miniblog / admin_miniblog / Ten96wabuQ10sztU)
+			try{
+				$bdd = new PDO('mysql:host=localhost;dbname=bdd_miniblog', 'root', '');
 			}
-	}
-?>
+			catch(Exception $e){
+				die('Erreur : '.$e->getMessage());
+			}
+
+			//Affichage de la page nécessaire
+			if (empty($_GET["page"])){
+				require_once "./controllers/userauth.php";
+			}else{
+				if ($_GET['page'] == 'articleslist') {
+						require_once "./controllers/articleslist.php";
+					}
+			}
+		?>
+		</body>
+	</html>
+
+
+
+
+
