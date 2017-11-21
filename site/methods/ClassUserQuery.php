6b73c386ae2df992727eb8385e30d54b0c9b72f2
kevzubrow <k.pean@aformac-vichy.fr>
2017-11-21 09:51:05 +0100

    derniere modif
    derniere modif essaie git

--- site/methods/ClassUserQuery.php
+++ site/methods/ClassUserQuery.php
@@ -1,3 +1,65 @@
 <?php
 
+class ClassUserQuery
+{
+	
+public function getUser($id){
+	private $_email;
+	private $_password;
+	private $_nom;
+	private $_prenom;
+	}
+
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+
+    public function setEmail($_email)
+    {
+        $this->_email = $_email;
+
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+
+    public function setPassword($_password)
+    {
+        $this->_password = $_password;
+
+        return $this;
+    }
+
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+
+
+    public function setNom($_nom)
+    {
+        $this->_nom = $_nom;
+
+        return $this;
+    }
+
+    public function getPrenom()
+    {
+        return $this->_prenom;
+    }
+
+
+    public function setPrenom($_prenom)
+    {
+        $this->_prenom = $_prenom;
+
+        return $this;
+    }
+}
 ?>
